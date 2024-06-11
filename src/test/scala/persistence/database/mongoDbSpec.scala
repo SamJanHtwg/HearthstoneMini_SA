a56797a7732ba8351b2ea09ddea4b2d66feb63b5
Jan <janaminger@googlemail.com>
2024-06-11 17:39:08 +0200

    WIP: mongoDbSpec

--- src/test/scala/persistence/database/mongoDbSpec.scala
+++ src/test/scala/persistence/database/mongoDbSpec.scala
@@ -2,53 +2,56 @@ package persistence.database.mongodb
 
 import _root_.model.fieldComponent.FieldInterface
 import _root_.persistence.database.mongodb.MongoDatabase as MyDataBase
-import com.mongodb.client.result.InsertOneResult
-import org.mockito.ArgumentCaptor
-import org.mockito.ArgumentMatchers.any
-import org.mockito.Mockito.*
+import com.mongodb.client.result.{DeleteResult, InsertOneResult, UpdateResult}
+import org.mockito.ArgumentMatchers.{any, eq => eqTo}
+import org.mockito.Mockito._
 import org.mongodb.scala._
 import org.mongodb.scala.bson.BsonInt32
+import org.scalatest.BeforeAndAfterEach
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import play.api.libs.json.Json
 
-import scala.concurrent.duration.*
+import scala.concurrent.ExecutionContext.Implicits.global
+import org.mockito.Mockito
 import scala.concurrent.Future
-import scala.util.Try
-import org.scalatest.BeforeAndAfterEach
-import com.mongodb.client.result.DeleteResult
-import com.mongodb.client.result.UpdateResult
 
 class MongoDatabaseTest
     extends AnyFlatSpec
     with Matchers
     with BeforeAndAfterEach {
-  // Mock dependencies
+
   var mockMongoCollection: MongoCollection[Document] = _
   var mockField: FieldInterface = _
-  var singleObservable: SingleObservable[InsertOneResult] = _
+  var singleInsertObservable: SingleObservable[InsertOneResult] = _
+  var singleDeleteObservable: SingleObservable[DeleteResult] = _
+  var singleUpdateObservable: SingleObservable[UpdateResult] = _
+  var mockFindObservable: FindObservable[Document] = _
 
   override def beforeEach(): Unit = {
     super.beforeEach()
     mockField = mock(classOf[FieldInterface])
-    singleObservable = SingleObservable(
-      InsertOneResult.acknowledged(BsonInt32(5))
-    )
+    singleInsertObservable = mock(classOf[SingleObservable[InsertOneResult]])
+    singleDeleteObservable = mock(classOf[SingleObservable[DeleteResult]])
+    singleUpdateObservable = mock(classOf[SingleObservable[UpdateResult]])
     mockMongoCollection = mock(classOf[MongoCollection[Document]])
+    mockFindObservable = mock(classOf[FindObservable[Document]])
 
     // Setup mock behavior
     when(mockMongoCollection.insertOne(any[Document]))
-      .thenReturn(singleObservable)
-    when(mockField.toJson).thenReturn(Json.obj("key" -> "value"))
-    // Stubbing for deleteOne and updateOne methods
+      .thenReturn(singleInsertObservable)
     when(mockMongoCollection.deleteOne(any[Document]))
-      .thenReturn(SingleObservable(DeleteResult.acknowledged(1)))
+      .thenReturn(singleDeleteObservable)
     when(mockMongoCollection.updateOne(any[Document], any[Document]))
-      .thenReturn(SingleObservable(UpdateResult.acknowledged(1, 1, null)))
+      .thenReturn(singleUpdateObservable)
+    when(mockField.toJson).thenReturn(Json.obj("key" -> "value"))
+    when(mockMongoCollection.find(any[Document])(any(), any()))
+      .thenReturn(mockFindObservable)
+    when(mockFindObservable.headOption())
+      .thenReturn(Future.successful(Some(Document("game" -> """{"key":"value"}"""))))
   }
 
   "MongoDatabase.save" should "insert a document into the collection" in {
-
     val dataBase = MyDataBase
     dataBase.collection = mockMongoCollection
 
@@ -58,6 +61,7 @@ class MongoDatabaseTest
     // verify
     verify(mockMongoCollection).insertOne(any[Document])
   }
+
   "MongoDatabase.load" should "return a JsValue" in {
     val dataBase = MyDataBase
     dataBase.collection = mockMongoCollection
@@ -66,7 +70,8 @@ class MongoDatabaseTest
     val result = dataBase.load()
 
     // verify
-    result.isSuccess shouldBe false
+    result.isSuccess shouldBe true
+    result.get.toString should include(""""key":"value"""")
   }
 
   "MongoDatabase.delete" should "delete a document from the collection" in {
@@ -74,9 +79,10 @@ class MongoDatabaseTest
     dataBase.collection = mockMongoCollection
 
     // execute
-    dataBase.delete()
+    val result = dataBase.delete()
 
     // verify
+    result.isSuccess shouldBe true
     verify(mockMongoCollection).deleteOne(any[Document])
   }
 
